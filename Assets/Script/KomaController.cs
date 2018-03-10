67719c88b65f29fbdab7a3f4ebfffb43669a14c1
rskmoi <zn5ps5@gmail.com>
2018-03-11 00:09:38 +0900

    持ち駒実装

--- Assets/Script/KomaController.cs
+++ Assets/Script/KomaController.cs
@@ -4,56 +4,56 @@ using UnityEngine;
 using System.Linq;
 
 public class KomaController : MonoBehaviour {
-    public Transform target;
-    int count;
-    private int[] ary;
-
-    private GameObject ousho;
-    private GameObject hisya;
-    private GameObject kaku;
-    private GameObject kin;
-    private GameObject gin;
-    private GameObject kei;
-    private GameObject kyou;
-    private GameObject fu;
-    
+    private int count;
     private Placements placements;
     private List<GameObject> komaList;
-    System.Random r = new System.Random(1);
-
+  
     // Use this for initialization
     void Start () {
-        count = 0;
-        ousho = GameObject.Find("OUSHO_01");
-        hisya = GameObject.Find("HISHA_01");
-        kaku = GameObject.Find("KAKU_01");
-        kin = GameObject.Find("KIN_01");
-        gin = GameObject.Find("GIN_01");
-        kei = GameObject.Find("KEIMA_01");
-        kyou = GameObject.Find("KYOSHA_01");
-        fu = GameObject.Find("FU_01");
-        komaList = new List<GameObject>
+        this.count = 0;
+        this.komaList = new List<GameObject>();
+
+        for(int i = 1; i < 3; i++)
         {
-            ousho,
-            hisya,
-            kaku,
-            kin,
-            gin,
-            kei,
-            kyou,
-            fu
-        };
+            var strNum = i.ToString().PadLeft(2, '0');
+            var ousho = GameObject.Find("OUSHO_" + strNum);
+            var hisha = GameObject.Find("HISHA_" + strNum);
+            var kaku = GameObject.Find("KAKU_" + strNum);
+            this.komaList.Add(ousho);
+            this.komaList.Add(hisha);
+            this.komaList.Add(kaku);
+        }
+
+        for (int i = 1; i < 5; i++)
+        {
+            var strNum = i.ToString().PadLeft(2, '0');
+            var kin = GameObject.Find("KIN_" + strNum);
+            var gin = GameObject.Find("GIN_" + strNum);
+            var kei = GameObject.Find("KEIMA_" + strNum);
+            var kyou = GameObject.Find("KYOSHA_" + strNum);
+            this.komaList.Add(kin);
+            this.komaList.Add(gin);
+            this.komaList.Add(kei);
+            this.komaList.Add(kyou);
+        }
+
+        for (int i = 1; i < 19; i++)
+        {
+            var strNum = i.ToString().PadLeft(2, '0');
+            var fu = GameObject.Find("FU_" + strNum);
+            komaList.Add(fu);
+        }
         placements = new Placements();
     }
 
     // Update is called once per frame
     void Update () {
         count++;
-        if(count % 50 == 0)
+        if (count % 50 == 0)
         {
-            this.placements.MakePlaceRandomly(8, komaList);
+            this.placements.MakePlaceRandomly(komaList);
             this.placements.Reset();
         }
-	}
+    }
 }
 
--- Assets/Script/Placements.cs
+++ Assets/Script/Placements.cs
@@ -7,8 +7,6 @@ using System.Linq;
 public class Placements
 {
     private List<PlacementInfo> placementList;
-    private int blackCapturedCount;
-    private int whiteCapturedCount;
     private int[] randomIndices;
     System.Random r = new System.Random(1);
 
@@ -16,48 +14,250 @@ public class Placements
     {
         this.placementList = new List<PlacementInfo>();
         InitializeBoard();
-        // InitializeKomadai();
-        this.blackCapturedCount = 0;
-        this.whiteCapturedCount = 0;
+        InitializeKomadai();
         InitializeRandomIndices();
     }
 
     /// <summary>
-    /// 今後の予定
-    /// MakePlaceRandomlyで持ち駒の位置情報もplaceMentlistに格納する
-    /// ↓
-    /// GetNextPlacementIndfoみたいなイテレーション的なメソッドをつくってとってってもらう
     /// </summary>
     /// <param name="komanumMax"></param>
     /// <param name="komaList"></param>
-    public void MakePlaceRandomly(int komanumMax, List<GameObject>  komaList)
+    public void MakePlaceRandomly(List<GameObject>  komaList)
     {
         ShuffleIndices();
-        var copiedRandomindices = new int[40];
-        Array.Copy(this.randomIndices, 0, copiedRandomindices, 0, 40);
+        var komaNum = komaList.Count();
+        var copiedRandomindices = new int[komaNum];
+        Array.Copy(this.randomIndices, 0, copiedRandomindices, 0, komaNum);
 
-        for(int komanum=0; komanum < komanumMax; komanum++)
+        var blackCapturedTable = new Dictionary<int, int>();
+        var whiteCapturedTable = new Dictionary<int, int>();
+
+        for (int komaIdx = 0; komaIdx < komaNum; komaIdx++)
         {
-            var randomIdx = copiedRandomindices[komanum];
-            komaList[komanum].transform.position = placementList[randomIdx].position;
-            komaList[komanum].transform.rotation = placementList[randomIdx].rotation;
+            var randomIdx = copiedRandomindices[komaIdx];
+            if (randomIdx < 81)
+            {
+                komaList[komaIdx].transform.position = this.placementList[randomIdx].position;
+                komaList[komaIdx].transform.rotation = this.placementList[randomIdx].rotation;
+            }
+            else if (randomIdx < 88)
+            {
+                blackCapturedTable.Add(komaIdx, randomIdx);
+            }
+            else
+            {
+                whiteCapturedTable.Add(komaIdx, randomIdx);
+            }
+        }
+
+        SetCaptured(blackCapturedTable.Values.ToList(), "Black");
+        SetCaptured(whiteCapturedTable.Values.ToList(), "White");
+
+        foreach (KeyValuePair<int, int> item in blackCapturedTable)
+        {
+            komaList[item.Key].transform.position = this.placementList[item.Value].position;
+            komaList[item.Key].transform.rotation = this.placementList[item.Value].rotation;
+        }
+
+        foreach (KeyValuePair<int, int> item in whiteCapturedTable)
+        {
+            komaList[item.Key].transform.position = this.placementList[item.Value].position;
+            komaList[item.Key].transform.rotation = this.placementList[item.Value].rotation;
         }
     }
 
     public void Reset()
     {
-        foreach (PlacementInfo plInfo in placementList)
+        foreach (PlacementInfo plInfo in this.placementList)
         {
             plInfo.Reset();
         }
-        this.blackCapturedCount = 0;
-        this.whiteCapturedCount = 0;
+    }
+
+    private void SetCaptured(List<int> capturedIdxList, string turn)
+    {
+        var capturedCount = capturedIdxList.Count();
+        var single_location = GetRandomLocations("All")[0];
+        var double_locations = GetRandomLocations("UpDown");
+        int rand = UnityEngine.Random.Range(0, 2);
+
+        if(capturedCount == 0)
+        {
+            return;
+        }
+
+        if (capturedCount < 5)
+        {
+            SetCapturedVecAndQtn(single_location, capturedIdxList, turn);
+            return;
+        }
+
+        switch (capturedCount)
+        {
+            case 5:
+                if(rand == 0)
+                {
+                    SetCapturedVecAndQtn(double_locations[0], capturedIdxList.GetRange(0, 1), turn);
+                    SetCapturedVecAndQtn(double_locations[1], capturedIdxList.GetRange(1, 4), turn);
+                }
+                else
+                {
+                    SetCapturedVecAndQtn(double_locations[0], capturedIdxList.GetRange(0, 2), turn);
+                    SetCapturedVecAndQtn(double_locations[1], capturedIdxList.GetRange(2, 3), turn);
+                }
+                break;
+            case 6:
+                if (rand == 0)
+                {
+                    SetCapturedVecAndQtn(double_locations[0], capturedIdxList.GetRange(0, 2), turn);
+                    SetCapturedVecAndQtn(double_locations[1], capturedIdxList.GetRange(2, 4), turn);
+                }
+                else
+                {
+                    SetCapturedVecAndQtn(double_locations[0], capturedIdxList.GetRange(0, 3), turn);
+                    SetCapturedVecAndQtn(double_locations[1], capturedIdxList.GetRange(3, 3), turn);
+                }
+                break;
+            case 7:
+                SetCapturedVecAndQtn(double_locations[0], capturedIdxList.GetRange(0, 3), turn);
+                SetCapturedVecAndQtn(double_locations[1], capturedIdxList.GetRange(3, 4), turn);
+                break;
+        }
+    }
+    
+    private string[] GetRandomLocations(string mode)
+    {
+        var baseLocations = new string[] {"up", "down", "middle" };
+        if(mode == "UpDown")
+        {
+            int randomVal = UnityEngine.Random.Range(0, 2);
+            return new string[] { baseLocations[randomVal], baseLocations[-(randomVal - 1)], baseLocations[2] };
+        }
+        else if(mode == "All")
+        {
+            return baseLocations.OrderBy(i => Guid.NewGuid()).ToArray();
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    private float GetZByLocation(string location, string turn)
+    {
+        if (location == "up")
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(-0.2f, 0.6f) : UnityEngine.Random.Range(-6.4f, -5.8f);
+        }
+        else if (location == "middle")
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(0.5f, 1.5f) : UnityEngine.Random.Range(-7.6f, -6.3f);
+        }
+        else if (location == "down")
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(1.6f, 2.4f) : UnityEngine.Random.Range(-8.4f, -7.5f);
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    private float GetXByCapturedCount(int capturedCount, string turn)
+    {
+        if (capturedCount == 1)
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(9.2f, 12.0f) : UnityEngine.Random.Range(26.9f, 29.9f);
+        }
+        else if (capturedCount == 2)
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(10.5f, 12.0f): UnityEngine.Random.Range(26.9f, 29.2f);
+        }
+        else if (capturedCount == 3)
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(11.2f, 12.0f) : UnityEngine.Random.Range(26.9f, 28.5f);
+        }
+        else if (capturedCount == 4)
+        {
+            return (turn == "Black") ? UnityEngine.Random.Range(11.5f, 12.0f) : UnityEngine.Random.Range(26.9f, 27.8f);
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    private float GetRotationYByCapturedCount(int capturedCount)
+    {
+        if (capturedCount == 1)
+        {
+            return UnityEngine.Random.Range(-5.0f, 5.0f);
+        }
+        else if (capturedCount == 2)
+        {
+            return UnityEngine.Random.Range(-2.0f, 8.0f);
+        }
+        else if (capturedCount == 3)
+        {
+            return UnityEngine.Random.Range(11.0f, 17.0f);
+        }
+        else if (capturedCount == 4)
+        {
+            return UnityEngine.Random.Range(14.0f, 21.0f);
+        }
+        else
+        {
+            throw new ArgumentException();
+        }
+    }
+
+    private void SetCapturedVecAndQtn(string location, List<int> capturedIdxList, string turn)
+    {
+        var rad18 = Mathf.Deg2Rad * 18.0f;
+
+        var x = GetXByCapturedCount(capturedIdxList.Count(), turn);
+        var y = 1.0f;
+        var z = GetZByLocation(location, turn);
+
+        var rotationX = 0;
+        var rotationY = GetRotationYByCapturedCount(capturedIdxList.Count());
+
+        var radY = Mathf.Deg2Rad * rotationY;
+
+        float centerX;
+        float centerZ;
+        if(turn == "Black")
+        {
+            centerX = x - 2.2f * Mathf.Sin(radY);
+            centerZ = z - 2.2f * Mathf.Cos(radY);
+        }
+        else
+        {
+            centerX = x + 2.2f * Mathf.Sin(radY);
+            centerZ = z + 2.2f * Mathf.Cos(radY);
+        }
+
+        var vec = new Vector3(x, y + 0.3f, z);
+        var conpensationRotationY = (turn == "Black") ? 180 : 0;
+        var qtn = Quaternion.Euler(rotationX + 180, rotationY + conpensationRotationY, 0);
+        foreach(int randomIdx in capturedIdxList)
+        {
+            this.placementList[randomIdx].position = vec;
+            this.placementList[randomIdx].rotation = qtn;
+            var newX = (x - centerX) * Mathf.Cos(rad18) - (z - centerZ) * Mathf.Sin(rad18) + centerX;
+            var newZ = (x - centerX) * Mathf.Sin(rad18) + (z - centerZ) * Mathf.Cos(rad18) + centerZ;
+            x = newX;
+            z = newZ;
+            rotationY -= 18.0f;
+            vec = new Vector3(x, y + 0.3f, z);
+            qtn = Quaternion.Euler(rotationX + 180, rotationY + conpensationRotationY, 0);
+        }
     }
 
     private void InitializeRandomIndices()
     {
-        this.randomIndices = new int[81];
-        for (int komaIdx = 0; komaIdx < 81; komaIdx++)
+        this.randomIndices = new int[95];
+        for (int komaIdx = 0; komaIdx < 95; komaIdx++)
         {
             randomIndices[komaIdx] = komaIdx;
         }
@@ -65,7 +265,7 @@ public class Placements
 
     private void ShuffleIndices()
     {
-        this.randomIndices = this.randomIndices.OrderBy(i => System.Guid.NewGuid()).ToArray();
+        this.randomIndices = this.randomIndices.OrderBy(i => Guid.NewGuid()).ToArray();
     }
 
     private void InitializeBoard()
@@ -88,6 +288,7 @@ public class Placements
             for(int i=0; i < 8; i++)
             {
                 var placementInfo = new PlacementInfo(turn);
+                placementList.Add(placementInfo);
             }
         }
     }
