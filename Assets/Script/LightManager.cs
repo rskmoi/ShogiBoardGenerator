eb13ef54a18dcb6466d308da4240fd27df6a7f23
rskmoi <zn5ps5@gmail.com>
2018-03-12 21:52:17 +0900

    LightManagerのrangeを確定

--- Assets/Script/LightManager.cs
+++ Assets/Script/LightManager.cs
@@ -16,9 +16,9 @@ public class LightManager
         var g = Random.Range(0.7f, 1.0f);
         var b = Random.Range(0.5f, 0.9f);
         this.lightObj.GetComponent<Light>().color = new Color(r, g, b, 1.0f);
-        var x = Random.Range(-20.0f, 20.0f);
+        var x = Random.Range(0.0f, 40.0f);
         var y = Random.Range(2.0f, 20.0f);
-        var z = Random.Range(-20.0f, 20.0f);
+        var z = Random.Range(-24.0f, 16.0f);
         this.lightObj.GetComponent<Light>().transform.position = new Vector3(x, y, z);
     }
 }
